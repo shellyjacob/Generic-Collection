6a15f5d65f48732e8d475980c8b08dd0b8680580
shellyjacob <sh3llyjacob@gmail.com>
2025-06-22 18:08:54 +0300

    Added handling for edge cases in functions 1 and 2.

--- Collection/Collection/FunctionsCollectionOperations.cs
+++ Collection/Collection/FunctionsCollectionOperations.cs
@@ -10,42 +10,59 @@ namespace Collection
     {
         public void ListOperations(List<string> list)
         {
-            if (list.Count() % 2 == 0)
+            if (list != null)
             {
-                list.RemoveAt(list.Count() / 2 - 1);
+                if (list.Count() > 0)
+                {
+                    if (list.Count % 2 == 0)
+                    {
+                        list.RemoveAt(list.Count / 2 - 1);
+                    }
+                    else
+                    {
+                        list.RemoveAt(list.Count / 2);
+                    }
+
+                    if (list.Contains("mama"))
+                    {
+                        list.Add("6");
+                    }
+
+                    if (list.IndexOf("course59") != -1 && list.IndexOf("course59") % 2 != 0)
+                    {
+                        list.Reverse();
+                    }
+
+                    if (list.Distinct().Count() == 3 && list.Count >= 2)
+                    {
+                        list.InsertRange(2, new List<string> { "2", "3", "4" });
+                    }
+                }
             }
             else
             {
-                list.RemoveAt(list.Count() / 2);
-            }
-
-            if (list.Contains("mama"))
-            {
-                list.Add("6");
-            }
-
-            if (list.IndexOf("course59") % 2 != 0)
-            {
-                list.Reverse();
-            }
-
-            if (list.Distinct().Count() == 3)
-            {
-                list.InsertRange(2, new List<string> {"2","3","4"});
+                throw new ArgumentNullException(nameof(list));
             }
         }
 
         public void DictionaryOperations(Dictionary<string, int> dictionary)
         {
-            if (dictionary.ContainsKey("scuba") && dictionary["scuba"] == 6)
+            if (dictionary != null)
             {
-                dictionary.Add("dive", 6);
+                if (dictionary.ContainsKey("scuba") && dictionary["scuba"] == 6 && !dictionary.ContainsKey("dive"))
+                {
+                    dictionary.Add("dive", 6);
+                }
             }
+            else
+            {
+                throw new ArgumentNullException(nameof(dictionary));
+            }
+            
         }
 
         public void StackOperations(Stack<DateTime> stack)
         {
-            throw new NotImplementedException();
         }
     }
 }
